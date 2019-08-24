6206ec47d1956a9c4f4de2c8e239bb01cfe27efc
Aashish Malethia <amalethia@gmail.com>
2019-08-24 12:47:04 +0530

    Updates

---
+++ precache-manifest.7df1e5d1b8da2cbacefb1d8a7352106b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ab4c2a503c1a932f75e46be9a030698",
+    "url": "/robo-react-redux/index.html"
+  },
+  {
+    "revision": "5b4d77abc40a01952fed",
+    "url": "/robo-react-redux/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "963408bd9e7fbaf43784",
+    "url": "/robo-react-redux/static/css/main.12cb220d.chunk.css"
+  },
+  {
+    "revision": "5b4d77abc40a01952fed",
+    "url": "/robo-react-redux/static/js/2.79e653cc.chunk.js"
+  },
+  {
+    "revision": "963408bd9e7fbaf43784",
+    "url": "/robo-react-redux/static/js/main.25520a9c.chunk.js"
+  },
+  {
+    "revision": "a1fbe380594829109b99",
+    "url": "/robo-react-redux/static/js/runtime~main.beb6343e.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robo-react-redux/static/media/SEGA.12a3db8b.woff"
+  }
+]);
